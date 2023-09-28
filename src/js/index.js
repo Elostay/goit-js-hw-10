89f177f77fb2869efde6a919f46c6320edc593ec
Elostay <nastia60203@gmail.com>
2023-09-28 16:00:20 +0300

    7

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 import Notiflix from 'notiflix';
 
 const select = document.querySelector('.breed-select');
